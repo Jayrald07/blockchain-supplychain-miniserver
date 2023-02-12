b15a530a3e20c89a225be2a495f4d54d4e10a2fa
Jayrald07 <geraldempino@gmail.com>
2023-02-13 07:43:41 +0800

    add local db

--- src/app.ts
+++ src/app.ts
@@ -2,26 +2,52 @@ import { exec } from "child_process";
 import express, { NextFunction } from "express";
 import cors from "cors";
 import getPort from "./getPort";
-
+import sql3, { sqlite3 } from "sqlite3";
 
 const app = express();
-
 
 app.use(cors({
   origin: "*"
 }))
+
 app.use(express.static("public"));
 
 app.use(express.json());
 
+const sqlite3 = sql3.verbose();
+
+let db = new sqlite3.Database("./src/config/configuration.db", (err) => {
+  if (err) console.error("Configuration Initialization Failed", err.message);
+
+  db.run("CREATE TABLE IF NOT EXISTS config (name TEXT, value TEXT)");
+
+
+  db.serialize(function () {
+    db.all("SELECT * FROM config WHERE name = \"SETUP\"", (error, rows: any[]) => {
+      if (error) console.error({ message: "Error", details: "Getting configuration failed" })
+      console.log("Config table fetched!")
+
+      if (!rows.length) {
+        let stmt = db.prepare("INSERT INTO config VALUES(?,?)");
+        stmt.run("SETUP", "processing");
+        stmt.finalize();
+      }
+    })
+  })
+
+})
+
+app.set("db", db);
+
+
 // This will be used for connecting the peer to main system to check if it is working and legit
 app.get("/ping", async (req: express.Request, res: express.Response, next: NextFunction) => {
 
-  res.send({ message: "Done", details: "pong", test: await getPort({}) });
+  res.send({ message: "Done", details: "pong" });
 });
 
 app.post("/initialize", async (req: express.Request, res) => {
-  const { orgName, username, password, msp } = req.body;
+  const { orgName, username, password, msp, id } = req.body;
 
   let port = await getPort({});
   let [operations, admin, general] = [await getPort({}), await getPort({}), await getPort({}),];
@@ -30,6 +56,20 @@ app.post("/initialize", async (req: express.Request, res) => {
     if (error) return res.send({ message: "Error initializing the peer", details: stderror, status: "error" });
     exec(`${process.cwd()}/../scripts/createOrderer.sh ${msp} ${general} ${admin} ${operations}`, (error, stdout, stderror) => {
       if (error) return res.send({ message: "Error initializing the peer", details: stderror, status: "error" });
+      let db: sql3.Database = app.get("db")
+
+      db.serialize(function () {
+        let stmt = db.prepare("INSERT INTO config VALUES(?,?)");
+        stmt.run("ID", id);
+        stmt.finalize();
+
+
+        stmt = db.prepare("UPDATE config SET value = ? WHERE name = \"SETUP\"");
+        stmt.run("done");
+        stmt.finalize();
+
+      })
+
       res.send({ message: "Done", details: { stdout, peerPort: port, ordererPorts: { general, admin, operations } } })
     })
   })
@@ -69,8 +109,18 @@ app.post("/joinOrg", async (req: express.Request, res: express.Response): Promis
 
 })
 
-app.get("/", (req, res) => {
-  res.sendFile(`${process.cwd}/public/index.html`);
+app.get("/getConfig", (req, res) => {
+
+  const db: sql3.Database = app.get("db")
+
+  db.serialize(function () {
+    db.all("SELECT * FROM config", (error, rows: any[]) => {
+      if (error) return res.send({ message: "Error", details: "Getting configuration failed" })
+      console.log(rows);
+      res.send({ message: "Done", details: rows })
+    })
+  })
+
 })
 
 // app.use((error: Error, req: express.Request, res: express.Response) => {
