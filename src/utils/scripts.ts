c3728b9276badc06fead0896f6f6f47c9bb99e0e
Jayrald07 <geraldempino@gmail.com>
2023-03-11 23:47:30 +0800

    update chaincode setup

--- src/utils/scripts.ts
+++ src/utils/scripts.ts
@@ -1,5 +1,9 @@
 export let scripts = {
-    packageChaincode: (version: string) => `peer lifecycle chaincode package mychannel.tar.gz --path /opt/gopath/src/github.com/hyperledger/fabric/peer/organizations --lang node --label mychannel_v${version}`,
-    installChaincode: () => "peer lifecycle chaincode install /opt/gopath/src/github.com/hyperledger/fabric/peer/organizations/mychannel.tar.gz",
-    queryInstalledChaincode: () => "peer lifecycle chaincode queryinstalled"
+    packageChaincode: "peer lifecycle chaincode package mychannel.tar.gz --path ./organizations --lang node --label mychannel_v${version}",
+    installChaincode: "peer lifecycle chaincode install ./organizations/mychannel.tar.gz",
+    queryInstalledChaincode: "peer lifecycle chaincode queryinstalled",
+    approveChaincode: "peer lifecycle chaincode approveformyorg -o $ORDERER_HOST:$ORDERER_GENERAL_PORT --ordererTLSHostnameOverride orderer.$ORG_NAME.com --tls --cafile $ORDERER_CA --name $CHAINCODE_NAME --channelID $CHANNEL_ID --version $VERSION --package-id $(peer lifecycle chaincode calculatepackageid ./organizations/mychannel.tar.gz) --sequence $SEQUENCE --waitForEvent --init-required --collections-config ./organizations/collections_config.json",
+    getCommitReadiness: "peer lifecycle chaincode checkcommitreadiness --channelID $CHANNEL_ID --name $CHAINCODE_NAME --version $VERSION --sequence $SEQUENCE --output json --init-required --collections-config ./organizations/collections_config.json",
+    commitChaincode: "peer lifecycle chaincode commit -o $ORDERER_HOST:$ORDERER_GENERAL_PORT --ordererTLSHostnameOverride orderer.$ORG_NAME.com --tls --cafile $ORDERER_CA --channelID $CHANNEL_ID --name $CHAINCODE_NAME --peerAddresses $HOST:$PEER_PORT --tlsRootCertFiles ./organizations/peerOrganizations/$ORG_NAME.com/peers/$ORG_NAME.com/tls/ca.crt --peerAddresses $EXTERNAL_HOST:$EXTERNAL_PEER_PORT --tlsRootCertFiles ./organizations/channel-artifacts/ca.crt --version $VERSION --sequence $SEQUENCE --init-required --collections-config ./organizations/collections_config.json",
+    initializeChaincode: "peer chaincode invoke -o $HOST:$ORDERER_GENERAL_PORT --ordererTLSHostnameOverride orderer.$ORG_NAME.com --tls --cafile $ORDERER_CA -C $CHANNEL_ID -n $CHAINCODE_NAME --peerAddresses $HOST:$PEER_PORT --tlsRootCertFiles ./organizations/peerOrganizations/$ORG_NAME.com/peers/$ORG_NAME.com/tls/ca.crt --peerAddresses $EXTERNAL_HOST:$EXTERNAL_PEER_PORT --tlsRootCertFiles ./organizations/channel-artifacts/ca.crt -c '{\"function\":\"InitLedger\",\"Args\":[]}' --isInit"
 }
